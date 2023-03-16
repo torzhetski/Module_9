fed804c2bb7cc23af391fa3a0ed674e7ed157b6a
Захар Торжецкий <torzhetski@mail.ru>
2023-03-16 20:08:00 +0300

    Добавьте файлы проекта.

---
+++ Task_1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+       
+        private void TextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (TextBox1.Text == "Введите текст...")
+            {
+                TextBox1.Clear();
+            }
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (TextBox1.Text == "Введите текст...")
+            {
+                TextBox1.Clear();
+            }
+        }
+
+        private void TextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (TextBox2.Text == "Введите текст...")
+            {
+                TextBox2.Clear();
+            }
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Spisok.Visibility=Visibility.Visible;
+            var text = module_5.Split(TextBox1.Text);
+            for (int i = 0; i < text.Length; i++)
+            {
+                Spisok.Items.Add(text[i]);
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            lable.Visibility=Visibility.Visible;
+            lable.Content = module_5.ReversPhrase(TextBox2.Text);
+        }
+
+       
+    }
+}
---
+++ Task_1/module_5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class module_5
+    {
+        /// <summary>
+        /// разделение строки на слова с помощью встроенной библиотеки и функции string.Split()
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <returns></returns>
+        public static string[] Split(string Text)
+        {
+            return Text.Split(new char[] { ' ' });
+
+        }
+        /// <summary>
+        /// размещение слов в обратном порядке
+        /// </summary>
+        /// <param name="Phrase"></param>
+        public static string ReversPhrase(string Phrase)
+        {
+            var splitedPhrase = Split(Phrase);
+            string reversPhrase = "";
+            for (int i = splitedPhrase.Length - 1; i >= 0; i--)
+            {
+                reversPhrase += splitedPhrase[i] + " ";
+            }
+           return reversPhrase;
+        }
+    }
+}
